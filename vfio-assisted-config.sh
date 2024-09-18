90ba719a2ada180fd1f7ed1533d4e9ad7cdecbc1
xanedarel <94392748+xanedarel@users.noreply.github.com>
2024-09-18 12:33:01 +0200

    Update vfio-assisted-config.sh

--- vfio-assisted-config.sh
+++ vfio-assisted-config.sh
@@ -2,7 +2,7 @@
 
 #checking dracut configuration folders and the status of a vfio.conf file
 dirdracut=/etc/dracut.conf.d/; ! [[ -d "$dirdracut" ]] && echo "script obsolete review dracut.conf.d" && exit || echo "dracut configuration folder found"
-confvfio=$(ls $dirdracut | grep vfio); [[ -z $confvfio ]] && echo "no prior vfio configuration found" || echo "existing vfio configuration found $confvfio"
+confvfio=$(ls $dirdracut | grep vfio); [[ -z $confvfio ]] && echo "no prior vfio configuration found, setting up $dirdracut/20-vfio.conf" || echo "existing vfio configuration found $confvfio"
 
 function iommuscript {
 ## iommu script from https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF#Enabling_IOMMU
@@ -26,19 +26,29 @@ done;
 #vga=$(lspci | grep VGA | awkprint)
 ! [[ -z $vga ]] && echo -e "The following GPUs have been found:\n$vga" || echo "Autodetection failed : Please enter your GPU manufacturer [Press enter for unknown]"
 
-#
+#get pci.ids values from the IOMMU group of target gpu pci device
 read vargpumkr
 if [ -z $vargpumkr ]; then
-echo "No manufacturer entered, displaying all IOMMU groups :" && wait 2 && iommuscript
+echo "No manufacturer entered, displaying all IOMMU groups :" && wait 2; iommuscript
 else
 iommuscript | grep -i $vargpumkr
 fi
 echo "Please enter the IOMMU group which you would like to passthrough:"
 read -p "IOMMU GROUP "  vargroup
-vargroupids=$(iommuscript | grep "IOMMU Group $vargroup" | grep -o "\[[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]:[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]]")
-echo "$vargroupids"
+vargroupids=$(iommuscript | grep "IOMMU Group $vargroup" | grep -o "[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]:[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]")
+
+varids=()
+for i in $vargroupids; do
+count=$((count + 1))
+varids+=($i); done
+
+echo ${varids[*]}
 #end config wip
-#write pci.ids to $confvfio
-#dracut -f
+#displaying pci.ids
+#echo "Found the following PCI IDS="
+#for ((i=1; i<=$count; i++)); do
+#eval echo "PCI ID $i: $varid$i"; done
+#echo $varid1
+#echo $varid2
 #sed -i 's/CMD_LINUX_DEFAULT=/""vfio-pci.ids=[ids]/g' /etc/default/grub
 #update-grub
