e8311d21dc63f4f15f047e467f2ebab91dcfb603
xanedarel <94392748+xanedarel@users.noreply.github.com>
2024-09-20 16:37:23 +0200

    Update vfio-assisted-config.sh
    fixed counters issues, added draft of array comparison

--- virtualisation/vfio-assisted-config.sh
+++ virtualisation/vfio-assisted-config.sh
@@ -16,7 +16,7 @@ if [ -z "$confvfio" ]; then
     echo "force_drivers+=\" vfio_pci vfio vfio_iommu_type1 \"" | tee $dirdracut/20-vfio.conf
     read -p "Regenerate initramfs now? [y/n]:" vargen; [[ "y" == "$vargen" ]] && dracut -f;
 else
-    echo "Existing vfio configuration found $confvfio"
+    echo "Existing vfio configuration found: $dirdracut/$confvfio"
 fi
 
 ##setting up the iommu script function
@@ -42,12 +42,12 @@ else
 fi
 echo "Please enter the IOMMU group which you would like to passthrough:"
 read -p "IOMMU GROUP "  vargroup
-vargrepids="[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]:[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]"
-vargroupids=$(iommuscript | grep "IOMMU Group $vargroup" | grep -o "$vargrepids")
+grepids="[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]:[0-9A-Za-z][0-9A-Za-z][0-9A-Za-z][0-9A-Za-z]"
+vargroupids=$(iommuscript | grep "IOMMU Group $vargroup" | grep -o "$grepids")
 
 ##creating array of pci ids
 arids=()
-for i in $vargroupids; do count=$((count + 1)); arids+=($i); done
+for i in $vargroupids; do countarids=$((countarids + 1)); arids+=($i); done
 
 ##write changes
 #check for user agreement
@@ -58,22 +58,25 @@ read -p "Continue with these settings? [y/n]:" vardoconfig
 #modify the array to fit grub's syntax
 varwriteids=$(echo ${arids[*]} | sed "s/ /,/g")
 #check wether /etc/default/grub already has pci.ids entries
-vargrubvfio=$(cat /etc/default/grub | grep -o "$vargrepids")
+vargrubvfio=$(cat /etc/default/grub | grep -o "$grepids")
 if [ -z "$vargrubvfio" ] ; then
     echo "Appending the ids to /etc/default/grub GRUB_CMDLINE_LINUX_DEFAULT line (requires root privileges)"
     sed -i "/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/\"$/ vfio-pci.ids=$varwriteids\"/" /etc/default/grub
     echo "Running update-grub"
     update-grub
 else
-else
-    agvfio=(); for i in $vargrubvfio; do countagvfio=$((count +1)); agvfio+=($i);done
-    #comparing arrays might make the condition expression return a false negative (todo)
-    if [[ "${arids[*]}" == "${agvfio[*]}" ]]; then echo "Configuration is already present with desired pci.ids"
-
-#fixing the array issue wip
-#for ((i=0; i<=$countarids; i++)); do ! [[ -z $(cat /etc/default/grub | grep ${arids[$i]}) ]] && eval var$i=found; done
-
-    else
-    echo "Found discrepency between script pci.ids and grub pci.ids: Grub=$vargrubvfio; Script=${agvfio[*]}"
-    fi
+#todo : redo
+    agvfio=(); for i in $vargrubvfio; do countagvfio=$((countagvfio +1)); agvfio+=($i);done
+idcheck=()
+    for ((i=0; i<$countarids; i++))
+        do declare grubids=$(cat /etc/default/grub | grep -o "${arids[$i]}" )
+        ! [[ -z "$grubids" ]] && idcheck+=(found) || idcheck+=(notfound)
+        done
+    for ((i=0; i<$countarids; i++))
+        do [[ "${idcheck[$i]}" == "found" ]] && echo "${agvfio[$i]} was found in preexisting /etc/default/grub"
+        [[ "${idcheck[$i]}" == "notfound" ]] && echo "Discrepency: ${agvfio[$i]} in /etc/default/grub. Expected: ${arids[$i]}"
+        done
+    for ((i=0; i<$countarids; i++))
+        do [[ ${idcheck[$i]} == "notfound" ]] &&
+# store in an array the values in /etc/default/grub that do not match $varwriteids
 fi
