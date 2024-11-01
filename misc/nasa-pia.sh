3d1235d041ff3dc00935c7da6c26b1221a668813
banepanel <94392748+xanedarel@users.noreply.github.com>
2024-11-02 00:21:31 +0100

    Create nasa-pia.sh

---
+++ misc/nasa-pia.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Purpose : when downloading images from nasa's library files can be named PIA or their filename
+# this script will run through all the images and compare classic filanames with PIA*
+# if the sha checksum is equal then it will delete the PIA file
+# it then runs a comparison whether the filename has a trailing [::space::] before its extension
+# if it's found it will remove it
+
+for i in ./*.tif; do
+	    root=$(sha1sum "$i" | awk -F ' ' '{print $1}')
+	if [ -n "$(ls ./PIA*.tif 2>/dev/null)" ]; then
+		for a in ./PIA*.tif; do
+		compare=$(sha1sum "$a" | awk -F ' ' '{print $1}')
+	    	[[ "$root" == "$compare" ]] && rm ./"$a"
+		done
+	fi
+# check for trailing [::space::] in filename	
+if [ -n "$(ls ./ | grep -E ".*[ ].tif")" <<< "$i" ]; then
+mv "$i" "$(sed -e 's/ .tif/.tif/g' <<< "$i" )"
+fi
+done
